2b0efc01ff47b3e89599dfef2376934b066ce269
Hugo Kinahan <hugokinahan@hotmail.com>
2021-06-30 12:12:22 +0100

    new background

--- src/App1.js
+++ src/App1.js
@@ -1,8 +1,10 @@
 import SubBox from './components/SubBox'
+// import background from './images/background-image.jpg'
 
 function App1() {
   return (
     <section>
+      {/* <img src={background} className='background'/> */}
       <SubBox />
     </section>
   )
